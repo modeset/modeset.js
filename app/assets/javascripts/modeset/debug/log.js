1b393782f7a249ca3ccab23275a7ff7436cf979e
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-02-17 00:17:40 -0700

    added html5 boilerplate log overriding

---
+++ app/assets/javascripts/modeset/debug/log.js
@@ -0,0 +1,8 @@
+// From HTML5 Boilerplate
+window.log = function() {
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if (this.console) {
+    console.log(Array.prototype.slice.call(arguments));
+  }
+};
