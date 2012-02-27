a834449c5af8f4bf6d4395e3002e3caeada292de
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-02-27 01:55:04 -0700

    rewrote ElasticPoint class and set up a demo page for it.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,11 +5,18 @@
 
 /* MATH */
 //= require modeset/math/math_util
+//= require modeset/math/elastic_point
 
 /* STRING */
 //= require modeset/string/formatter
 //= require modeset/string/path_util
 //= require modeset/string/string_util
+
+/* SCROLL */
+//= require modeset/scroll/cursor
+//= require modeset/scroll/css_helper
+//= require modeset/scroll/touch_tracker
+//= require modeset/scroll/touch_scroll
 
 /* APPLICATION ASSETS */
 /* require_tree ./components*/
--- app/assets/javascripts/modeset/math/elastic_point.js
+++ app/assets/javascripts/modeset/math/elastic_point.js
@@ -1,20 +1,35 @@
-function ElasticPoint( x, y, fric, accel ){
-	var self = this;
-	
-	// current location
-	this.curX = x;
-	this.curY = y;
-	this.targetX = x;
-	this.targetY = y;
-	this.xspeed = 0;
-	this.yspeed = 0;
-	
-	this.updatePosition = function() {
-		// elastically move based on current target poisition vs current position
-		self.xspeed = ((self.targetX-self.curX)*accel+self.xspeed)*fric;
-		self.yspeed = ((self.targetY-self.curY)*accel+self.yspeed)*fric;
-		self.curX += self.xspeed;
-		self.curY += self.yspeed;
-	};
+// usage: var point = new ElasticPoint( _box_base_x, _box_base_y, 0.75, 0.4 );
+
+function ElasticPoint( x, y, fric, accel ) {
+  var self = this;
+  
+  this.fric = fric;
+  this.accel = accel;
+  this.curX = x;
+  this.curY = y;
+  this.targetX = x;
+  this.targetY = y;
+  this.xspeed = 0;
+  this.yspeed = 0;
+
+  this.setTarget = function( x, y ) {
+    self.targetX = x;
+    self.targetY = y;
+  };
+  
+  this.setFriction = function( fric ) {
+    self.fric = fric;
+  };
+  
+  this.setAccel = function( accel ) {
+    self.accel = accel;
+  };
+  
+  this.updatePosition = function() {
+    // elastically move based on current target poisition vs current position
+    self.xspeed = ((self.targetX-self.curX)*self.accel+self.xspeed)*self.fric;
+    self.yspeed = ((self.targetY-self.curY)*self.accel+self.yspeed)*self.fric;
+    self.curX += self.xspeed;
+    self.curY += self.yspeed;
+  };
 }
-// var point = new ElasticPoint( _box_base_x, _box_base_y, 0.75, 0.4 );
