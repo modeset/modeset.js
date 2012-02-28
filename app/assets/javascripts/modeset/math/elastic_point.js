0ad9e268caaaa55dcc937cfaa082913cdd16f9c4
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-02-28 02:32:14 -0700

    converted ElasticPoint to module-pattern class

--- app/assets/javascripts/modeset/math/elastic_point.js
+++ app/assets/javascripts/modeset/math/elastic_point.js
@@ -1,35 +1,51 @@
-// usage: var point = new ElasticPoint( _box_base_x, _box_base_y, 0.75, 0.4 );
+var ElasticPoint = function( x, y, fric, accel ) {
+  _fric = fric;
+  _accel = accel;
 
-function ElasticPoint( x, y, fric, accel ) {
-  var self = this;
+  _curX = x;
+  _curY = y;
   
-  this.fric = fric;
-  this.accel = accel;
-  this.curX = x;
-  this.curY = y;
-  this.targetX = x;
-  this.targetY = y;
-  this.xspeed = 0;
-  this.yspeed = 0;
+  _targetX = x;
+  _targetY = y;
+  
+  _speedX = 0;
+  _speedY = 0;
 
-  this.setTarget = function( x, y ) {
-    self.targetX = x;
-    self.targetY = y;
+  var x = function() {
+    return _curX;
+  };
+
+  var y = function() {
+    return _curY;
+  };
+
+  var setTarget = function( x, y ) {
+    _targetX = x;
+    _targetY = y;
   };
   
-  this.setFriction = function( fric ) {
-    self.fric = fric;
+  var setFriction = function( fric ) {
+    _fric = fric;
   };
   
-  this.setAccel = function( accel ) {
-    self.accel = accel;
+  var setAccel = function( accel ) {
+    _accel = accel;
   };
   
-  this.updatePosition = function() {
-    // elastically move based on current target poisition vs current position
-    self.xspeed = ((self.targetX-self.curX)*self.accel+self.xspeed)*self.fric;
-    self.yspeed = ((self.targetY-self.curY)*self.accel+self.yspeed)*self.fric;
-    self.curX += self.xspeed;
-    self.curY += self.yspeed;
+  var updatePosition = function() {
+    // update elastic point based on current target position vs current position
+    _speedX = ( ( _targetX - _curX ) * _accel + _speedX ) * _fric;
+    _speedY = ( ( _targetY - _curY ) * _accel + _speedY ) * _fric;
+    _curX += _speedX;
+    _curY += _speedY;
+  };
+
+  return {
+    x: x,
+    y: y,
+    setTarget: setTarget,
+    setFriction: setFriction,
+    setAccel: setAccel,
+    updatePosition: updatePosition
   };
 }
