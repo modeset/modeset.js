5777c54cb4113bb684b93cc70168fe4a74d927f3
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-05 01:48:55 -0700

    added nicer css class manipulated methods

--- app/assets/javascripts/modeset/util/dom_util.js
+++ app/assets/javascripts/modeset/util/dom_util.js
@@ -108,6 +108,35 @@ DOMUtil.toggleClass = function( el, name, on ) {
   }
 };
 
+    function hasClassName(inElement, inClassName)
+    {
+        var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
+        return regExp.test(inElement.className);
+    }
+
+    function addClassName(inElement, inClassName)
+    {
+        if (!hasClassName(inElement, inClassName))
+            inElement.className = [inElement.className, inClassName].join(' ');
+    }
+
+    function removeClassName(inElement, inClassName)
+    {
+        if (hasClassName(inElement, inClassName)) {
+            var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
+            var curClasses = inElement.className;
+            inElement.className = curClasses.replace(regExp, ' ');
+        }
+    }
+
+    function toggleClassName(inElement, inClassName)
+    {
+        if (hasClassName(inElement, inClassName))
+            removeClassName(inElement, inClassName);
+        else
+            addClassName(inElement, inClassName);
+    }
+
 DOMUtil.replaceElement = function( oldElem, newElem ) {
   oldElem.parentNode.replaceChild( newElem, oldElem );
 };
