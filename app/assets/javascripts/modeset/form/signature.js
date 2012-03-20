4fd639d4fdff609f5b3c4d4fe084d1c74aac9718
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-19 22:24:33 -0600

    add smooth cubic bezier calculation method to implement in signature class

--- app/assets/javascripts/modeset/form/signature.js
+++ app/assets/javascripts/modeset/form/signature.js
@@ -261,6 +261,31 @@ var SignatureCanvas = function( canvasElem, debugging ) {
     _context.fillRect(_canvas_height - 1, _canvas_width - cornerLine, 1, cornerLine);
 	};
 
+  // original from Robin W. Spencer (http://scaledinnovation.com)
+function getControlPoints(x0,y0,x1,y1,x2,y2,t){
+    //  x0,y0,x1,y1 are the coordinates of the end (knot) pts of this segment
+    //  x2,y2 is the next knot -- not connected here but needed to calculate p2
+    //  p1 is the control point calculated here, from x1 back toward x0.
+    //  p2 is the next control point, calculated here and returned to become the 
+    //  next segment's p1.
+    //  t is the 'tension' which controls how far the control points spread.
+    
+    //  Scaling factors: distances from this knot to the previous and following knots.
+    var d01=Math.sqrt(Math.pow(x1-x0,2)+Math.pow(y1-y0,2));
+    var d12=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+   
+    var fa=t*d01/(d01+d12);
+    var fb=t-fa;
+  
+    var p1x=x1+fa*(x0-x2);
+    var p1y=y1+fa*(y0-y2);
+
+    var p2x=x1-fb*(x0-x2);
+    var p2y=y1-fb*(y0-y2);  
+    
+    return [p1x,p1y,p2x,p2y]
+}
+
 	var dispose = function() {
     _touch_tracker.dispose();
     _context = null;
