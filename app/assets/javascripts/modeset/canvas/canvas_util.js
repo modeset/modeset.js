6f1e9ba149fde96e10d2ca8a3e3b996d5f3fd25c
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-19 22:28:05 -0600

    add canvas_util class to convert

---
+++ app/assets/javascripts/modeset/canvas/canvas_util.js
@@ -0,0 +1,23 @@
+// original from Robin W. Spencer (http://scaledinnovation.com)
+function hexToCanvasColor(hexColor,opacity){
+    // Convert #AA77CC to rbga() format for Firefox
+    opacity=opacity || "1.0";
+    hexColor=hexColor.replace("#","");
+    var r=parseInt(hexColor.substring(0,2),16);
+    var g=parseInt(hexColor.substring(2,4),16);
+    var b=parseInt(hexColor.substring(4,6),16);
+    return "rgba("+r+","+g+","+b+","+opacity+")";
+}
+
+// original from Robin W. Spencer (http://scaledinnovation.com)
+function drawPoint(ctx,x,y,r,color){
+    ctx.save();  
+    ctx.beginPath();
+    ctx.lineWidth=1;
+    ctx.fillStyle=hexToCanvasColor(color,1);
+    ctx.arc(x,y,r,0.0,2*Math.PI,false);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
+}
