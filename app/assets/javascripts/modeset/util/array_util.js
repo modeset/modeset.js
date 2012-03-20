3ef49dc3a065ed4ffa54f380c868e7bbc1305850
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-19 22:24:49 -0600

    add eliminateDuplicates() in array_util

--- app/assets/javascripts/modeset/util/array_util.js
+++ app/assets/javascripts/modeset/util/array_util.js
@@ -27,3 +27,19 @@ ArrayUtil.indexOf = function(array, obj) {
   }
   return -1;
 };
+
+
+function eliminateDuplicates(arr) {
+  var i,
+      len=arr.length,
+      out=[],
+      obj={};
+
+  for (i=0;i<len;i++) {
+    obj[arr[i]]=0;
+  }
+  for (i in obj) {
+    out.push(i);
+  }
+  return out;
+}
