2b3b843e486bf0e58d130a34bc4ae7cc8d96bfad
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-22 17:52:50 -0600

    added GetImageSize class to asynchronously obtain the image size from loaded or unloaded images

---
+++ app/assets/javascripts/modeset/media/image/get_image_size.js
@@ -0,0 +1,29 @@
+GetImageSize = function( src, callback ) {
+  this.src = src;
+  this.callback = callback;
+  this.image = new Image();
+  this.requestImageSize()
+};
+
+GetImageSize.prototype.requestImageSize = function () { 
+  var self = this;
+  // create load/error callbacks
+  this.image.onload = function () {
+    if( self.callback ) self.callback( self.image.width, self.image.height );
+    self.cleanup();
+  };
+  this.image.onerror = function () {
+    if( self.callback ) self.callback( -1, -1 );
+    self.cleanup();
+  };
+  // load it
+  this.image.src = this.src;
+};
+
+GetImageSize.prototype.cleanup = function() {
+  delete this.image.onload;
+  delete this.image.onerror;
+  delete this.image;
+  delete this.src;
+  delete this.callback;
+};
