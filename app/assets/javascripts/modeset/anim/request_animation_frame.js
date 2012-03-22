8477b2830cc51639188005ca2762edcc4ab7943d
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-22 17:53:43 -0600

    remove iOS-killing time calculation for now

--- app/assets/javascripts/modeset/anim/request_animation_frame.js
+++ app/assets/javascripts/modeset/anim/request_animation_frame.js
@@ -16,7 +16,7 @@
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function(callback, element) {
             var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var timeToCall = 16;//Math.max(0, 16 - (currTime - lastTime));
             var id = window.setTimeout(function() { callback(currTime + timeToCall); },
               timeToCall);
             lastTime = currTime + timeToCall;
