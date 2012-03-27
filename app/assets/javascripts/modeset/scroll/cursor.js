34052fa53e2217987b07f10454c958870d831b81
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-27 17:46:03 -0600

    major cleanup on cursor.js. styles are now externalized in _cursor-grab.css. also conditional use of jquery/zepto if it's there, otherwise fallback to DOMUtil for class add/remove

--- app/assets/javascripts/modeset/scroll/cursor.js
+++ app/assets/javascripts/modeset/scroll/cursor.js
@@ -1,81 +1,61 @@
 /* 
- * Adds grabby-hand cursor functionality on-demand
- * Absolute paths to .cur files are needed for IE
+ * Adds grabby-hand cursor functionality on-demand, and handles cases for different browsers
+ * Absolute paths to .cur files are needed for IE. Chrome likes the cursor files too
+ * TODO: IE might only want the .cur style def, and not want the plain css class
  */
-function Cursor( openHandCursor, closedHandCursor ){
-  openHandCursor = openHandCursor || '/images/cursors/openhand.cur';
-  closedHandCursor = closedHandCursor || '/images/cursors/closedhand.cur';
-  
+function Cursor( element ){
   this.is_chrome = !!navigator.userAgent.toLowerCase().match(/chrome/i);
   this.is_msie = !!navigator.userAgent.toLowerCase().match(/msie/i);
   this.is_iphone = !!navigator.userAgent.toLowerCase().match(/iphone/i);
-  this.element = document.body;
-  this.base_inline_css = this.element.getAttribute("style") || '';
-  
-  this.CSS_HAND = 'cursor:hand; cursor:grab; cursor:-moz-grab; cursor:-webkit-grab;';
-	this.CSS_HAND_CUR = 'cursor: url(' + openHandCursor + '), default !important;';
-	this.CSS_HAND_GRAB = 'cursor:grabbing; cursor:-moz-grabbing; cursor:-webkit-grabbing;';
-	this.CSS_HAND_GRAB_CUR = 'cursor: url(' + closedHandCursor + '), default !important;';
+  this.element = element || document.body;
 }
 
 Cursor.prototype.cursorSetDefault = function() {
-  if( !this.is_msie ) {
-    this.element.setAttribute('style', this.base_inline_css);
+  if( this.is_chrome || this.is_msie ) {
+    this.removeClass( 'hand handCursor' );
+    this.removeClass( 'handGrab handGrabCursor' );
   } else { 
-    DOMUtil.removeClass( this.element, 'ie_hand' );
-    DOMUtil.removeClass( this.element, 'ie_hand_grab' );
+    this.removeClass( 'hand' );
+    this.removeClass( 'handGrab' );
   }
 };
 
 Cursor.prototype.cursorSetHand = function() {
-  if( this.is_chrome ) {
-    this.element.setAttribute('style', this.base_inline_css + this.CSS_HAND + this.CSS_HAND_CUR);
-  } else if( this.is_msie ) {
-    DOMUtil.removeClass( this.element, 'ie_hand_grab' );
-    DOMUtil.addClass( this.element, 'ie_hand' );
+  this.cursorSetDefault();
+  if( this.is_chrome || this.is_msie ) {
+    this.addClass( 'hand handCursor' );
   } else {
-    this.element.setAttribute('style', this.base_inline_css + this.CSS_HAND);
+    this.addClass( 'hand' );
   }
 };
 
 Cursor.prototype.cursorSetGrabbyHand = function() {
-  if( this.is_chrome ) {
-    this.element.setAttribute('style', this.base_inline_css + this.CSS_HAND_GRAB + this.CSS_HAND_GRAB_CUR);
-  } else if( this.is_msie ) {
-    DOMUtil.removeClass( this.element, 'ie_hand' );
-    DOMUtil.addClass( this.element, 'ie_hand_grab' );
-    // IE hack for updating cursor after mousedown - actually just creates an error that repaints the cursor
-    //document.execCommand("SelectAll");
-    //document.body.focus();
-    //document.selection.clear()
-    setTimeout(function(){
-      //document.selection.clear()
-      //document.execCommand("Unselect");
-      
-    },50);
-    
-    //qtfMate();
-    // window.blur();
-    // window.focus();
-    //window.resizeBy(1,0); window.resizeBy(-1,0);    
+  this.cursorSetDefault();
+  if( this.is_chrome || this.is_msie ) {
+    this.addClass( 'handGrab handGrabCursor' );
   } else {
-    this.element.setAttribute('style', this.base_inline_css + this.CSS_HAND_GRAB);
+    this.addClass( 'handGrab' );
   }
+};
+
+Cursor.prototype.addClass = function( className ) {
+  if($)
+    $(this.element).addClass( className );
+  else
+    DOMUtil.addClass( className );
+};
+
+Cursor.prototype.removeClass = function( className ) {
+  if($)
+    $(this.element).removeClass( className );
+  else
+    DOMUtil.removeClass( className );
 };
 
 Cursor.prototype.dispose = function(){
   this.cursorSetDefault();
-  
-  this.is_chrome = false;
-  this.is_msie = false;
-  this.is_iphone = false;
-  this.element = false;
-  this.base_inline_css = false;
-  
-  this.CSS_HAND = false;
-	this.CSS_HAND_CUR = false;
-	this.CSS_HAND_GRAB = false;
-	this.CSS_HAND_GRAB_CUR = false;
-  
+  delete this.is_chrome;
+  delete this.is_msie;
+  delete this.is_iphone;
+  delete this.element;
 };
-
