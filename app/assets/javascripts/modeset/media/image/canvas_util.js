0970474bf4348372851d28c5e1256ab0ef233815
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-06-05 01:10:07 -0600

    fix up methods and comments

--- app/assets/javascripts/modeset/media/image/canvas_util.js
+++ app/assets/javascripts/modeset/media/image/canvas_util.js
@@ -25,26 +25,27 @@ CanvasUtil.hexToCanvasColor = function( hexColor, opacity ) {
 
 /**
  *  Draws a filled circle. Original code from Robin W. Spencer (http://scaledinnovation.com).
- *  @use    {@code CanvasUtil.drawPoint('#00ff00', 0.5);}
+ *  @use    {@code CanvasUtil.drawPoint( context, 50, 50, 40 );}
  */
-CanvasUtil.drawPoint = function( ctx, x, y, r, color ) {
+CanvasUtil.drawPoint = function( ctx, x, y, radius ) {
   ctx.save();
   ctx.beginPath();
-  ctx.lineWidth = 1;
-  ctx.fillStyle = CanvasUtil.hexToCanvasColor( color, 1 );
-  ctx.arc( x, y, r, 0.0, 2 * Math.PI, false );
+  ctx.arc( x, y, radius, 0.0, 2 * Math.PI, false );
   ctx.closePath();
   ctx.stroke();
   ctx.fill();
   ctx.restore();
 };
 
-
+/**
+ *  Draws an arc - a portion of a circle.
+ *  @use    {@code CanvasUtil.drawArc(context, 50, 50, 40, 90, 180);}
+ */
 CanvasUtil.drawArc = function( ctx, x, y, radius, startAngle, endAngle ) {
   ctx.save();
   ctx.beginPath();
-  ctx.translate( x, y );
-  ctx.arc( 0, 0, radius, (Math.PI / 180) * (startAngle - 90), (Math.PI / 180) * (endAngle - 90), false );
+  ctx.arc( x, y, radius, (Math.PI / 180) * (startAngle - 90), (Math.PI / 180) * (endAngle - 90), false );
+  ctx.closePath();
   ctx.stroke();
   ctx.fill();
   ctx.restore();
