ebf3fb81f332a89968b0a4c6989bae4be5498470
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-06-04 18:44:18 -0600

    adding CanvasUtil methods & cleaning up

--- app/assets/javascripts/modeset/media/image/canvas_util.js
+++ app/assets/javascripts/modeset/media/image/canvas_util.js
@@ -1,7 +1,7 @@
 var CanvasUtil = CanvasUtil || {};
 
 /**
- *  Calculates a random number within a minimum and maximum range.
+ *  Lets a developer know if the canvas element is supported.
  *  @return A boolean indicating canvas support.
  *  @use    {@code CanvasUtil.hasCanvas();}
  */
@@ -9,41 +9,43 @@ CanvasUtil.hasCanvas = function() {
   return (!document.createElement('canvas').getContext) ? false : true;
 };
 
-// // original from Robin W. Spencer (http://scaledinnovation.com)
-// function hexToCanvasColor(hexColor,opacity){
-//     // Convert #AA77CC to rbga() format for Firefox
-//     opacity=opacity || "1.0";
-//     hexColor=hexColor.replace("#","");
-//     var r=parseInt(hexColor.substring(0,2),16);
-//     var g=parseInt(hexColor.substring(2,4),16);
-//     var b=parseInt(hexColor.substring(4,6),16);
-//     return "rgba("+r+","+g+","+b+","+opacity+")";
-// }
+/**
+ *  Converts a hex color value to canvas-friendly rgba. Original code from Robin W. Spencer (http://scaledinnovation.com).
+ *  @return An rgba color string.
+ *  @use    {@code CanvasUtil.hexToCanvasColor('#00ff00', 0.5);}
+ */
+CanvasUtil.hexToCanvasColor = function( hexColor, opacity ) {
+  opacity = ( opacity != null ) ? opacity : "1.0";
+  hexColor = hexColor.replace( "#", "" );
+  var r = parseInt( hexColor.substring( 0, 2 ), 16 );
+  var g = parseInt( hexColor.substring( 2, 4 ), 16 );
+  var b = parseInt( hexColor.substring( 4, 6 ), 16 );
+  return "rgba("+r+","+g+","+b+","+opacity+")";
+};
 
-// // original from Robin W. Spencer (http://scaledinnovation.com)
-// function drawPoint(ctx,x,y,r,color){
-//     ctx.save();
-//     ctx.beginPath();
-//     ctx.lineWidth=1;
-//     ctx.fillStyle=hexToCanvasColor(color,1);
-//     ctx.arc(x,y,r,0.0,2*Math.PI,false);
-//     ctx.closePath();
-//     ctx.stroke();
-//     ctx.fill();
-//     ctx.restore();
-// }
+/**
+ *  Draws a filled circle. Original code from Robin W. Spencer (http://scaledinnovation.com).
+ *  @use    {@code CanvasUtil.drawPoint('#00ff00', 0.5);}
+ */
+CanvasUtil.drawPoint = function( ctx, x, y, r, color ) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 1;
+  ctx.fillStyle = CanvasUtil.hexToCanvasColor( color, 1 );
+  ctx.arc( x, y, r, 0.0, 2 * Math.PI, false );
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore();
+};
 
-// function drawArc(startAngle,endAngle) {
-//   var drawingArc = true;  // set boolean value to show we&rsquo;re drawing
-//   // Define our path using the API
-//   ctx.beginPath();
-//   ctx.arc(15,15,10, (Math.PI/180)*(startAngle-90),(Math.PI/180)*(endAngle-90), false);
-//   ctx.stroke();   // draw on the canvas
-//   drawingArc = false;
-// }
 
-//     drawArc = (ctx,radius,startAngle,endAngle) ->
-//       ctx.beginPath()
-//       ctx.arc( 0, 0, radius, (Math.PI/180)*(startAngle-90),(Math.PI/180)*(endAngle-90), false)
-//       ctx.stroke()
-
+CanvasUtil.drawArc = function( ctx, x, y, radius, startAngle, endAngle ) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.translate( x, y );
+  ctx.arc( 0, 0, radius, (Math.PI / 180) * (startAngle - 90), (Math.PI / 180) * (endAngle - 90), false );
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore();
+};
