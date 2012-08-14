6bdd9ce7dceab21e7d86958a4e392e1b2db2fd27
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-08-14 01:37:54 -0600

    Add script to add Android classes to the document

---
+++ app/assets/javascripts/modeset/mobile/android-fix.js
@@ -0,0 +1,14 @@
+!function(){
+  var is_android = ( navigator.userAgent.toLowerCase().match(/android/i) ) ? true : false;
+  var elem = document.documentElement;
+  if( is_android == true ) {
+    elem.className = [elem.className, 'android'].join(' ');
+    // check for android 4+ so we can hardware accelerate
+    var androidVersion = parseFloat( navigator.userAgent.match(/Android (\d+(?:\.\d+)+)/gi)[0].replace('Android ','') )
+    if( androidVersion >= 4 ) {
+      elem.className = [elem.className, 'android4plus'].join(' ');
+    }
+  } else {
+    elem.className = [elem.className, 'no-android'].join(' ');
+  }
+}();
