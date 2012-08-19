4c9ccbcf172fe635d70cce8a80935196814b35d3
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-08-18 22:22:21 -0600

    Add new popup-blocker-avoiding new window method

--- app/assets/javascripts/modeset/mobile/mobile-util.js
+++ app/assets/javascripts/modeset/mobile/mobile-util.js
@@ -53,3 +53,14 @@ MobileUtil.hideSoftKeyboard = function() {
   document.activeElement.blur()
   $('input').blur()
 };
+
+MobileUtil.openNewWindow = function( href ) {
+  // gets around native popup blockers
+  var link = document.createElement('a');
+  link.setAttribute('href', href);
+  link.setAttribute('target','_blank');
+  var clickevent = document.createEvent('Event');
+  clickevent.initEvent('click', true, false);
+  link.dispatchEvent(clickevent);
+  return false;
+};
