579625e6298804d5606f87fb321b278727b8eb05
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-08-28 01:15:04 -0600

    Clean up & reorganize some files

--- app/assets/javascripts/modeset/util/mouse_util.js
+++
@@ -1,9 +0,0 @@
-function MouseUtil() {}
-
-MouseUtil.getRelativeXFromEvent = function( $event, $relativeElement ){
-    var curleft = $relativeElement.offsetLeft;
-    while($relativeElement = $relativeElement.offsetParent) {
-      curleft += $relativeElement.offsetLeft;
-    }
-    return $event.clientX - curleft;
-}
